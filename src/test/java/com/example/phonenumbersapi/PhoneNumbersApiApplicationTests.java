853fc1b4a9b8de2fbe9d742ac4464da480abccb6
tikhon <25050056@study.bsuir.by>
2024-02-19 22:44:48 +0300

    i fixed code smell again )

--- src/test/java/com/example/phonenumbersapi/PhoneNumbersApiApplicationTests.java
+++ src/test/java/com/example/phonenumbersapi/PhoneNumbersApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.phonenumbersapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class PhoneNumbersApiApplicationTests {
-
+	@Autowired
+	private ApplicationContext context;
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
